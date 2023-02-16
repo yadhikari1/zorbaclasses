762741ba6a81243a1dac08c2895ef2d013c560b9
yadhikari1 <yadhikari86@gmail.com>
2023-02-16 17:49:19 -0500

    first

---
+++ one/day1.java
@@ -0,0 +1,99 @@
+package one;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+		
+		// Installation
+		// variables??
+		// Type
+		//Extrovert -- speaks more , more social , more outing
+		//Introvert -- calm , speaks less , less outing
+		
+		// Types
+	
+		// Human
+		// Property - age , weight height color
+		// Methods - talk() , walk()
+		
+		// Vehicle 
+		// Property -color type brand model number 
+		// Method - start() , stop()
+		
+		// Bank type
+		// Property - accName , accNumber , balance , IFSC code 
+		// Method - Deposit() , withdrawl()
+		
+		int x = 10; // 700 , -89 , 0
+		
+		
+		// Define a variable
+		
+		int s = 10;
+		int t = 5;
+		
+		// Arithmetic operation
+		
+		// + , - , * , / ,%
+		// + -> addition 
+		// - -> subtraction
+		// * -> multiplication
+		// / -> division 
+		// % -> modulus
+		
+		
+		System.out.println(s+t);
+		System.out.println(s-t);
+		System.out.println(s*t);
+		System.out.println(s/t);
+		System.out.println(s%t);
+		
+		
+		int a = 90;
+		int b = 45;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		
+		// 10 pairs ?? 50 lines
+		
+		Calculator(12,6);
+		Calculator(10,3);
+	}
+	
+	
+	static void Calculator(int x, int y) {
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		
+	}
+	
+	public static int Calculator2(int x , int y) {
+			return x+ y;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
